c3498b73885e369a06e4beb92ce8014c94ef7f0a
suhyeon.jeon <suhyeon.jeon@fig.xyz>
2025-07-11 16:54:28 +0900

    project setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -10,7 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier", // Prettier와 충돌 방지
+  ),
+  {
+    plugins: {
+      prettier: prettierPlugin, // Prettier 플러그인 추가
+    },
+    rules: {
+      "prettier/prettier": "error", // Prettier 규칙 위반 시 ESLint 에러 발생
+      "@next/next/no-img-element": "off", // next/image 규칙 비활성
+    },
+  },
 ];
 
 export default eslintConfig;
